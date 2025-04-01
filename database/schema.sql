848949e26c1d69f134f332bbe3c2c2c9a441ce0e
JenniferCoSt <jennifer.cor.alm@outlook.com>
2025-04-01 15:24:26 +0200

    Added comments and updated creation of tables

--- database/schema.sql
+++ database/schema.sql
@@ -1,14 +1,18 @@
+-- ClimateCode Database Schema
+-- Create database and tables for climate data from SMHI
 drop database if exists ClimateCode;
 create database ClimateCode;
 use ClimateCode;
 
-create table MeasurementParameters(
+-- Table for different measurement parameters (temperature, rain etc)
+create table if not exists MeasurementParameters(
 id int not null auto_increment primary key,
 parameterType varchar(20) not null,
 unit varchar(10) not null
 );
 
-create table Stations(
+-- Table for information about stations measuring the data
+create table if not exists Stations(
 id int not null auto_increment primary key,
 stationName varchar(50),
 stationNumber int,
@@ -17,7 +21,8 @@ dataSource varchar(30) not null,
 metersAboveSeaLevel float
 );
 
-create table Measurements(
+-- Table for measurement data 
+create table if not exists Measurements(
 id int not null auto_increment primary key,
 stationId int,
 measurementParameterId int,
@@ -32,6 +37,7 @@ foreign key (stationId) references Stations(id) on delete set null,
 foreign key (measurementParameterId) references MeasurementParameters(id) on delete set null
 );
 
+-- Indexes to optimize different searches
 create index IX_measurementDate on Measurements(dataDate);
 create index IX_measurementStation on Measurements(stationId);
 create index IX_measurementParameters on Measurements(measurementParameterId);

e6932e11fdd6bf971f0a760c3b83d0ad6b8680f0
Jennifer Cordova Almonacid <jennifer.cor.alm@outlook.com>
2025-04-06 14:45:22 +0200

    Added setters, getters and constructors

--- src/main/java/com/JenniferCoSt/ClimateCode/Stations.java
+++ src/main/java/com/JenniferCoSt/ClimateCode/Stations.java
@@ -13,4 +13,64 @@ public class Stations {
     private String location;
     private String dataSource;
     private int metersAboveSeaLevel;
+
+
+    public Stations() {}
+
+    public Stations(int id, String stationName, String stationCode, String location, String dataSource, int metersAboveSeaLevel) {
+        this.id = id;
+        this.stationName = stationName;
+        this.stationCode = stationCode;
+        this.location = location;
+        this.dataSource = dataSource;
+        this.metersAboveSeaLevel = metersAboveSeaLevel;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getStationName() {
+        return stationName;
+    }
+
+    public void setStationName(String stationName) {
+        this.stationName = stationName;
+    }
+
+    public String getStationCode() {
+        return stationCode;
+    }
+
+    public void setStationCode(String stationCode) {
+        this.stationCode = stationCode;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getDataSource() {
+        return dataSource;
+    }
+
+    public void setDataSource(String dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    public int getMetersAboveSeaLevel() {
+        return metersAboveSeaLevel;
+    }
+
+    public void setMetersAboveSeaLevel(int metersAboveSeaLevel) {
+        this.metersAboveSeaLevel = metersAboveSeaLevel;
+    }
 }
